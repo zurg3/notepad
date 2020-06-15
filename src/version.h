12dfe5829187549a95c0a358b3aef018fc7f6f2b
zurg3 <20664125+zurg3@users.noreply.github.com>
2020-06-15 16:26:32 +0300

    v0.5.1 - Fixed unavailable undo action when insert current time and date

--- src/version.h
+++ src/version.h
@@ -1,12 +1,12 @@
 #include <QString>
 #include <QSysInfo>
 
-constexpr auto app_version = "0.5";
+constexpr auto app_version = "0.5.1";
 
 constexpr auto app_version_major = 0;
 constexpr auto app_version_minor = 5;
-constexpr auto app_version_patch = 0;
+constexpr auto app_version_patch = 1;
 
-constexpr auto app_build = "71";
+constexpr auto app_build = "72";
 
 const QString app_full_version = app_version + QString(" (") + app_build + QString(")") + QString(" ") + QSysInfo::buildCpuArchitecture();
